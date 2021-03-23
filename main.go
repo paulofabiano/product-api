e8b618f9263ebd7a8c01255e1209b10ff6dbdd23
Paulo Langer <paulo.langer@xing.com>
2021-03-23 13:03:43 +0000

    Creating handlers package, customizing server with timeouts and notify os.Signals for Shutdown.

--- main.go
+++ main.go
@@ -1,28 +1,48 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	"github.com/paulofabiano/product-api/handlers"
 )
 
 func main() {
-	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
-		log.Println("Hello World!")
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
 
-		d, err := ioutil.ReadAll(r.Body)
+	hh := handlers.NewHello(l)
+	gh := handlers.NewGoodbye(l)
+
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
+	sm.Handle("/goodbye", gh)
+
+	s := &http.Server{
+		Addr:         ":9090",
+		Handler:      sm,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+
+	go func() {
+		err := s.ListenAndServe()
 		if err != nil {
-			http.Error(rw, "Ops", http.StatusBadRequest)
-			return
+			l.Fatal(err)
 		}
+	}()
 
-		fmt.Fprintf(rw, "Hello %s", d)
-	})
+	sigChain := make(chan os.Signal)
+	signal.Notify(sigChain, os.Interrupt)
+	signal.Notify(sigChain, os.Kill)
 
-	http.HandleFunc("/goodbye", func(http.ResponseWriter, *http.Request) {
-		log.Println("Goodbye World!")
-	})
+	sig := <-sigChain
+	l.Println("Received terminate, graceful shutdown", sig)
 
-	http.ListenAndServe(":9090", nil)
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
 }
