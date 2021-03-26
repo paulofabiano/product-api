7d4309c10479e134ba5e4f7d23daf28d3de72e72
Paulo Langer <paulo.langer@xing.com>
2021-03-26 20:48:47 +0000

    handling POST requests

--- handlers/products.go
+++ handlers/products.go
@@ -21,6 +21,11 @@ func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	if r.Method == http.MethodPost {
+		p.addProduct(rw, r)
+		return
+	}
+
 	// catch all
 	rw.WriteHeader(http.StatusMethodNotAllowed)
 }
@@ -31,4 +36,18 @@ func (p *Products) getProducts(rw http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
 	}
+}
+
+func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle POST Products")
+
+	prod := &data.Product{}
+	err := prod.FromJSON(r.Body)
+
+	if err != nil {
+		http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
+	}
+
+	p.l.Printf("Prod: %#v", prod)
+	data.AddProduct(prod)
 }
